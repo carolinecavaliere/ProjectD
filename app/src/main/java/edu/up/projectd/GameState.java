d12df74a7c03b48d248e82ee26a8a690e0448557
carolinecavaliere <cavalier23@up.edu>
2020-10-03 14:28:15 -0700

    GameState constructor (needs to deal cards still)

--- app/src/main/java/edu/up/projectd/GameState.java
+++ app/src/main/java/edu/up/projectd/GameState.java
@@ -11,19 +11,26 @@ public class GameState {
     int p3numCards;
     int p4numCards;
 
+    ArrayList<Card> p1Hand;
+    ArrayList<Card> p2Hand;
+    ArrayList<Card> p3Hand;
+    ArrayList<Card> p4Hand;
+
     int turn;
 
-    ArrayList<Card> p1TopCards = new ArrayList<Card>();
-    ArrayList<Card> p1BottomCards = new ArrayList<Card>();
+    DeckOfCards deck;
 
-    ArrayList<Card> p2TopCards = new ArrayList<Card>();
-    ArrayList<Card> p2BottomCards = new ArrayList<Card>();
+    ArrayList<Card> p1TopCards;
+    ArrayList<Card> p1BottomCards;
 
-    ArrayList<Card> p3TopCards = new ArrayList<Card>();
-    ArrayList<Card> p3BottomCards = new ArrayList<Card>();
+    ArrayList<Card> p2TopCards;
+    ArrayList<Card> p2BottomCards;
 
-    ArrayList<Card> p4TopCards = new ArrayList<Card>();
-    ArrayList<Card> p4BottomCards = new ArrayList<Card>();
+    ArrayList<Card> p3TopCards;
+    ArrayList<Card> p3BottomCards;
+
+    ArrayList<Card> p4TopCards;
+    ArrayList<Card> p4BottomCards;
 
     Card drawPileTopCard;
     Card playPileTopCard;
@@ -35,15 +42,37 @@ public class GameState {
     {
         this.numPlayers = numPlayers;
         int turn = 1;
-        drawPileNumCards = 0;
         playPileNumCards = 0;
         p1numCards = 0;
         p2numCards = 0;
         p3numCards = 0;
         p4numCards = 0;
+        p1TopCards = new ArrayList<Card>();
+        p1BottomCards = new ArrayList<Card>();
+
+        p2TopCards = new ArrayList<Card>();
+        p2BottomCards = new ArrayList<Card>();
+
+        p3TopCards = new ArrayList<Card>();
+        p3BottomCards = new ArrayList<Card>();
+
+        p4TopCards = new ArrayList<Card>();
+        p4BottomCards = new ArrayList<Card>();
+
+        p1Hand = new ArrayList<Card>();
+        p2Hand = new ArrayList<Card>();
+        p3Hand = new ArrayList<Card>();
+        p4Hand = new ArrayList<Card>();
+
         if(this.numPlayers==2)
         {
-
+            deck = new DeckOfCards(1);
         }
+        else
+        {
+            deck= new DeckOfCards(2);
+        }
+        drawPileNumCards = deck.deck.size();
+
     }
 }
