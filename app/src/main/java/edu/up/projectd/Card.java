0460ded484a583daa133d8934deda642b6cbbbd4
Chloe Gan <chloegwyneth4@gmail.com>
2020-10-03 12:56:43 -0700

    card class

---
+++ app/src/main/java/edu/up/projectd/Card.java
@@ -0,0 +1,14 @@
+package edu.up.projectd;
+
+public class Card {
+    String name;
+    int suit;
+    int rank;
+
+    //public Card (card orig) {
+        //this.name = orig.name;
+        //this.sut = orig.suit;
+        //this.rank = orig.rank;
+
+    //}
+}
