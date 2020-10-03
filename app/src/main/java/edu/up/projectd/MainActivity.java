923e92a2f4c5828005966ecebf6078033c798b95
Nathaniel <pon23@up.edu>
2020-10-03 13:42:59 -0700

    onClick part 1

--- app/src/main/java/edu/up/projectd/MainActivity.java
+++ app/src/main/java/edu/up/projectd/MainActivity.java
@@ -3,12 +3,29 @@ package edu.up.projectd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Button runTest = findViewById(R.id.RunTest);
+    EditText text = findViewById(R.id.EditText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runTest.setOnClickListener(new ButtonListener());
+
+    }
+
+    private class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            text.setText("");
+            text.getText().clear();
+        }
     }
 }
