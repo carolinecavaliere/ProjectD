011e386a0aa664121d4b829662c7ea9ff5ed17cb
Nathaniel <pon23@up.edu>
2020-10-03 13:02:02 -0700

    GameState

--- app/src/main/java/edu/up/projectd/Card.java
+++ app/src/main/java/edu/up/projectd/Card.java
@@ -1,14 +1,12 @@
 package edu.up.projectd;
 
 public class Card {
-    String name;
     int suit;
     int rank;
 
-    //public Card (card orig) {
-        //this.name = orig.name;
-        //this.sut = orig.suit;
-        //this.rank = orig.rank;
+    public Card (Card orig) {
+        this.suit = orig.suit;
+        this.rank = orig.rank;
 
-    //}
+    }
 }
---
+++ app/src/main/java/edu/up/projectd/GameState.java
@@ -0,0 +1,21 @@
+package edu.up.projectd;
+
+public class GameState {
+    int p1numCards;
+    int p2numCards;
+    int p3numCards;
+    int p4numCards;
+
+    int p1TopCard1;
+    int p1TopCard2;
+    int p1TopCard3;
+    int p2TopCard1;
+    int p2TopCard2;
+    int p2TopCard3;
+    int p3TopCard1;
+    int p3TopCard2;
+    int p3TopCard3;
+    int p4TopCard1;
+    int p4TopCard2;
+
+}
