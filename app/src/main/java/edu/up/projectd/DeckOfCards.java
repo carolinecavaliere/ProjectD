40c4e5178be9460f95a6312cb4e0bf329ec6a4a0
jimih <hayesj23@up.edu>
2020-10-03 13:43:59 -0700

    temp deck

---
+++ app/src/main/java/edu/up/projectd/DeckOfCards.java
@@ -0,0 +1,12 @@
+package edu.up.projectd;
+
+import java.util.ArrayList;
+
+public class DeckOfCards {
+    ArrayList<Card> deck;
+    DeckOfCards(int numDecks){
+        for(int i = 0; i<numDecks; i++){
+            deck.add(new Card(14, ))
+        }
+    }
+}
