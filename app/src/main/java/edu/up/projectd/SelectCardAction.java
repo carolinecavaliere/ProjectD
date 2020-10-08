451eb03ddea8857d2cbe9a7e1cc3f5e52f614212
Chloe Gan <chloegwyneth4@gmail.com>
2020-10-08 15:17:36 -0700

    added getters and setters to gamestate

---
+++ app/src/main/java/edu/up/projectd/SelectCardAction.java
@@ -0,0 +1,21 @@
+package edu.up.projectd;
+
+public class SelectCardAction {
+
+    public boolean selectCard(int playerId, GameState state, Card card) {
+        if (playerId != state.turn) {
+
+        } else if (playerId == 2) {
+
+        } else if (playerId == 3) {
+
+        } else if (playerId == 4) {
+
+        } else {
+            return false;
+
+        }
+        return false;
+    }
+
+}
