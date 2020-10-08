f9b45ac3fd8ccd9d8769a2b641e4a58dab501ecb
Nathaniel <pon23@up.edu>
2020-10-08 15:46:35 -0700

    SwitchBaseCards and TakePile. playPileCards added to GameState

---
+++ app/src/main/java/edu/up/projectd/TakePile.java
@@ -0,0 +1,46 @@
+package edu.up.projectd;
+
+import java.util.ArrayList;
+
+public class TakePile {
+
+    public boolean takePile(int playerID, GameState gameState) {
+        ArrayList<Card> card = gameState.getPlayPileCards();
+        if (gameState.getPlayPileNumCards() > 0 && playerID == gameState.getTurn()) {
+            if (playerID == 1) {
+                for (int i = 0; i < gameState.getPlayPileNumCards(); i++) {
+                    card.add(gameState.getPlayPileCards().get(i));
+                }
+                gameState.setP1Hand(card);
+                return true;
+            }
+            else if (playerID == 2) {
+                for (int i = 0; i < gameState.getPlayPileNumCards(); i++) {
+                    card.add(gameState.getPlayPileCards().get(i));
+                }
+                gameState.setP2Hand(card);
+                return true;
+            }
+            else if (playerID == 3) {
+                for (int i = 0; i < gameState.getPlayPileNumCards(); i++) {
+                    card.add(gameState.getPlayPileCards().get(i));
+                }
+                gameState.setP3Hand(card);
+                return true;
+            }
+            else if (playerID == 4) {
+                for (int i = 0; i < gameState.getPlayPileNumCards(); i++) {
+                    card.add(gameState.getPlayPileCards().get(i));
+                }
+                gameState.setP4Hand(card);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+}
