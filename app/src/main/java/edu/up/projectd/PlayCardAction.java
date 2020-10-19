bbfb45e6460ac5a6114f03f4d08b0fafba541b76
carolinecavaliere <cavalier23@up.edu>
2020-10-18 22:53:51 -0700

    PlayCardAction condense.

--- app/src/main/java/edu/up/projectd/PlayCardAction.java
+++ app/src/main/java/edu/up/projectd/PlayCardAction.java
@@ -1,98 +1,34 @@
+
 package edu.up.projectd;
 
 import java.util.ArrayList;
 
 public class PlayCardAction {
     public boolean playCard(int playerId, ArrayList<Card> cardsToPlay, GameState state) {
-        if (state.getTurn() != playerId) {
+        if (state.getTurn() != playerId)//not a valid move if it is not that player's turn
+        {
             return false;
-        } else if (state.getCardSelected() == false) {
+        }
+        else if (state.getCardSelected() == false)//not a valid move if a card isn't selected
+        {
             return false;
-        } else if (cardsToPlay.size() > 1) {
-            for (int i = 0; i < cardsToPlay.size() - 1; i++) {
-                if (cardsToPlay.get(i).getRank() != cardsToPlay.get(i + 1).getRank()) {
+        }
+        else if (cardsToPlay.size() > 1) //not a valid move if multiple cards are selected but not the same rank
+        {
+            for (int i = 0; i < cardsToPlay.size() - 1; i++)
+            {
+                if (cardsToPlay.get(i).getRank() != cardsToPlay.get(i + 1).getRank())
+                {
                     return false;
                 }
             }
-            for (int i = 0; i < cardsToPlay.size(); i++) {
-                ArrayList<Card> temp;
-                ArrayList<Card> tempHand;
-                if (playerId == 1) {
-                    tempHand = new ArrayList<Card>(state.getP1Hand());
-                    temp = new ArrayList<Card>(state.getPlayPileCards());
-                    temp.add(cardsToPlay.get(i));
-                    tempHand.remove(cardsToPlay.get(i));
-                    state.setP1Hand(temp);
-                    state.setP1Hand(tempHand);
-                    state.setPlayPileCards(temp);
-                } else if (playerId == 2) {
-                    tempHand = new ArrayList<Card>(state.getP2Hand());
-                    temp = new ArrayList<Card>(state.getPlayPileCards());
-                    temp.add(cardsToPlay.get(i));
-                    tempHand.remove(cardsToPlay.get(i));
-                    state.setP2Hand(temp);
-                    state.setP2Hand(tempHand);
-                    state.setPlayPileCards(temp);
-                } else if (playerId == 3) {
-                    tempHand = new ArrayList<Card>(state.getP3Hand());
-                    temp = new ArrayList<Card>(state.getPlayPileCards());
-                    temp.add(cardsToPlay.get(i));
-                    tempHand.remove(cardsToPlay.get(i));
-                    state.setP3Hand(temp);
-                    state.setP3Hand(tempHand);
-                    state.setPlayPileCards(temp);
-                } else if (playerId == 4) {
-                    tempHand = new ArrayList<Card>(state.getP4Hand());
-                    temp = new ArrayList<Card>(state.getPlayPileCards());
-                    temp.add(cardsToPlay.get(i));
-                    tempHand.remove(cardsToPlay.get(i));
-                    state.setP4Hand(temp);
-                    state.setP4Hand(tempHand);
-                    state.setPlayPileCards(temp);
-                }
-                return true;
-            }
+        }
+        else {
             for (int i = 0; i < cardsToPlay.size(); i++) {
                 if (cardsToPlay.get(i).getRank() < state.getPlayPileTopCard().getRank()) {
                     if (cardsToPlay.get(i).getRank() != 2 && cardsToPlay.get(i).getRank() != 10) {
-                        return false;
-                    } else {
-                        ArrayList<Card> temp;
-                        ArrayList<Card> tempHand;
-                        if (playerId == 1) {
-                            tempHand = new ArrayList<Card>(state.getP1Hand());
-                            temp = new ArrayList<Card>(state.getPlayPileCards());
-                            temp.add(cardsToPlay.get(i));
-                            tempHand.remove(cardsToPlay.get(i));
-                            state.setP1Hand(temp);
-                            state.setP1Hand(tempHand);
-                            state.setPlayPileCards(temp);
-                        } else if (playerId == 2) {
-                            tempHand = new ArrayList<Card>(state.getP2Hand());
-                            temp = new ArrayList<Card>(state.getPlayPileCards());
-                            temp.add(cardsToPlay.get(i));
-                            tempHand.remove(cardsToPlay.get(i));
-                            state.setP2Hand(temp);
-                            state.setP2Hand(tempHand);
-                            state.setPlayPileCards(temp);
-                        } else if (playerId == 3) {
-                            tempHand = new ArrayList<Card>(state.getP3Hand());
-                            temp = new ArrayList<Card>(state.getPlayPileCards());
-                            temp.add(cardsToPlay.get(i));
-                            tempHand.remove(cardsToPlay.get(i));
-                            state.setP3Hand(temp);
-                            state.setP3Hand(tempHand);
-                            state.setPlayPileCards(temp);
-                        } else if (playerId == 4) {
-                            tempHand = new ArrayList<Card>(state.getP4Hand());
-                            temp = new ArrayList<Card>(state.getPlayPileCards());
-                            temp.add(cardsToPlay.get(i));
-                            tempHand.remove(cardsToPlay.get(i));
-                            state.setP4Hand(temp);
-                            state.setP4Hand(tempHand);
-                            state.setPlayPileCards(temp);
-                        }
-                        return true;}
+                        return false;//not a valid move if the card is less than the one on the play pile and not of rank 2 or 10
+                    }
                 } else {
                     ArrayList<Card> temp;
                     ArrayList<Card> tempHand;
