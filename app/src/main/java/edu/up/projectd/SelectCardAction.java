337fd65f9c5da0bb3dcd294585ee31d7bb7be817
Chloe Gan <chloegwyneth4@gmail.com>
2020-10-19 17:39:26 -0700

    used new add and remove methods for selectedcard method

--- app/src/main/java/edu/up/projectd/SelectCardAction.java
+++ app/src/main/java/edu/up/projectd/SelectCardAction.java
@@ -14,23 +14,19 @@ public class SelectCardAction {
             return false;
         } else if (playerId == 1 && state.getP1Hand().contains(card)){  // looks at player 1's hand
             selectedCard.add(card); // add the card object to the arraylist
-            state.setSelectedCards(selectedCard); // put arraylist into GameState variable
-            /*for (int i = 0; i < state.getP1Hand().size(); i++) { // iterate thru their hand
-                selectedCard = state.getP1Hand().get(cardIndex); // looks in hand, gets that card index
-                state.setSelectedCards(selectedCard); // put into variable
-            }*/
+            state.addToSelectedCards((Card) selectedCard); // put arraylist into GameState variable
             return true;
         } else if (playerId == 2 && state.getP2Hand().contains(card)){  // looks at player 2's hand
             selectedCard.add(card);
-            state.setSelectedCards(selectedCard); // put into GameState variable
+            state.addToSelectedCards((Card) selectedCard); // put into GameState variable
             return true;
         } else if (playerId == 3 && state.getP3Hand().contains(card)){  // looks at player 3's hand
             selectedCard.add(card);
-            state.setSelectedCards(selectedCard); // put into GameState variable
+            state.addToSelectedCards((Card) selectedCard); // put into GameState variable
             return true;
         } else if (playerId == 4 && state.getP4Hand().contains(card)){  // looks at player 4's hand
             selectedCard.add(card);
-            state.setSelectedCards(selectedCard); // put into GameState variable
+            state.addToSelectedCards((Card) selectedCard); // put into GameState variable
             return true;
         }
         return false;
